1438eab1da02e1f64bf62456545c6b9cde5a1d41
stellarboymihir <stellarboymihir00@gmail.com>
2024-08-19 23:43:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hello from './components/Hello'
+import Bye from './components/Bye'
+
+function App() {
+  
+
+  return (
+   <>
+   <Hello/>
+   <Bye/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,9 @@
+
+
+function Hello(){
+    return (
+        <h1>Hello from Hello Component</h1>
+    )
+}
+
+export default Hello
